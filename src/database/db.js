2e0c9ef375aef8a4ffa5d579f443e64c08b104a8
Ihor Kliushnyk <igorator746@gmail.com>
2025-05-05 20:12:54 +0300

    refactor(db): comment out synchronize option in DataSource configuration

--- src/database/db.js
+++ src/database/db.js
@@ -24,6 +24,6 @@ export const AppDataSource = new DataSource({
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   entities: [Gift, User],
-  synchronize: true,
+  //synchronize: true,
   logging: true,
 });
