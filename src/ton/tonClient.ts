5c4b672d27d3c4537d14d920832a3f69a882d6c8
Ihor Kliushnyk <igorator746@gmail.com>
2025-05-25 20:07:47 +0300

    refactor: remove unused contract files and update tonClient configuration for environment variables

--- src/ton/tonClient.ts
+++ src/ton/tonClient.ts
@@ -1,5 +1,11 @@
+import 'dotenv/config';
 import { TonClient } from '@ton/ton';
 
+if (!process.env.TONCENTER_API_ENDPOINT) {
+  throw new Error('❌ TONCENTER_API_ENDPOINT is not defined');
+}
+
 export const tonClient = new TonClient({
-  endpoint: 'https://toncenter.com/api/v2/jsonRPC',
+  endpoint: process.env.TONCENTER_API_ENDPOINT,
+  apiKey: process.env.TONCENTER_API_KEY,
 });
